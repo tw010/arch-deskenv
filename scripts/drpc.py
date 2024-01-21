c522274889e7ea6aac98cdcec8d81d6c5918ec5c
tw010 <145844618+tw010@users.noreply.github.com>
2024-01-21 18:38:01 +0200

    drpc?

--- scripts/drpc.py
+++ scripts/drpc.py
@@ -1,4 +1,4 @@
-import DiscordRPC
+from pypresence import Presence
 import time
 import subprocess
 
@@ -8,24 +8,22 @@ host = subprocess.run("uname -n", shell=True, capture_output=True, check=True)
 user = user.stdout.decode('utf-8').replace("\n", "")
 host = host.stdout.decode('utf-8')
 
-time.sleep(4)
+rpc = Presence("1133070138602700810")
 
 while True:
-    try:
-        rpc = DiscordRPC.RPC.Set_ID(app_id=1133070138602700810)
-        
-        while True:
-            uptime = subprocess.run("uptime -p", shell=True, capture_output=True, check=True)
-            uptime = uptime.stdout.decode('utf-8')
-            rpc.set_activity(
-                state = uptime,
-                details = user+"@"+host,
-                large_image = "arch",
-                small_image = "linux",
-                small_text = "linux"
-            )
-            time.sleep(8)
-    except:
-        print("discord disabled, sleeping")
-        time.sleep(16)
-        continue
+	try:
+		rpc.connect()
+		while True:
+			uptime = subprocess.run("uptime -p", shell=True, capture_output=True, check=True)
+			uptime = uptime.stdout.decode('utf-8')
+			print(rpc.update(
+				state = uptime,
+				details = user+"@"+host,
+				large_image = "arch",
+				small_image = "linux",
+				small_text = "linux"
+			))
+			time.sleep(4)
+	except:
+		print("discord disabled/error")
+		continue
